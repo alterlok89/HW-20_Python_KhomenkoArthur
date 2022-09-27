4122340e16aaca50c076e560a6c3763785f77c04
alterlo89 <alterlok89@gmail.com>
2022-09-27 18:00:41 +0300

    + HW-21

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import time
 from threading import Thread
 import requests
 from bs4 import BeautifulSoup
+import sqlite3
 
-# Задание. Парсинг сайта
+# Задание. Парсинг сайта и Базы данных
 
 
 def auto_parcing(page):
@@ -21,6 +22,7 @@ def auto_parcing(page):
 
     response = requests.get(url)
     if response.status_code == 200:
+        adresses = []
         soup = BeautifulSoup(response.text, 'lxml')
             # для нахождения названий авто
         items_href = soup.find_all('a', class_='address', )
@@ -42,22 +44,39 @@ def auto_parcing(page):
 
 def auto_page(adress):
     url = adress
-    auto_url.append(url)
     response_auto = requests.get(url)
     if response_auto.status_code == 200:
         auto_soup = BeautifulSoup(response_auto.text, 'lxml')
         item_name = auto_soup.find('h1', class_="head",)
-        car_name.append(item_name.text)
-        # print(url)
-        # print(item_name.text)
+        # print(item_name)
         item_price = auto_soup.find('strong', class_="",)
-        prices.append(item_price.get_text())
-        # print(item_price.get_text())
+        # print(item_price)
         item_mileage = auto_soup.find('div', class_="base-information bold",)
-        mileage.append(item_mileage.get_text())
-        # print(item_mileage.get_text())
+        # print(item_mileage)
+        if item_name != None:
+            auto_url.append(url)
+            car_name.append(item_name.text)
+            # print(item_name.text)
+            prices.append(item_price.get_text())
+            # print(item_price.get_text())
+            mileage.append(item_mileage.get_text())
+            # print(item_mileage.get_text())
 
 
+
+dict = {}
+prices = []
+car_name = []
+mileage = []
+auto_url = []
+
+start = time.perf_counter()
+pages = 2  # в моем случае 78 страниц
+for page in range(0, pages):
+    auto_parcing(page)
+end = time.perf_counter()
+print(f'time = {end - start:0.2f} s')
+print(len(car_name), len(mileage), len(prices), len(auto_url),)
 
 dict = {}
 adresses = []
@@ -67,13 +86,8 @@ mileage = []
 auto_url = []
 threads = []
 
-
-# так и не понял как вытянуть из автория количество страниц.
-# такое ощущение что там то ли защита стоит,
-# то ли как-то по другому надо дааные получать,
-# т.к. постоянно получал пустые данные
 start = time.perf_counter()
-pages = 78  # в моем случае 78 страниц
+pages = 2  # в моем случае 78 страниц
 for page in range(0, pages):
     t = Thread(target=auto_parcing(page), args=(page,))
     t.start()
@@ -81,20 +95,82 @@ for page in range(0, pages):
 
 for t in threads:
     t.join()
-    
 end = time.perf_counter()
 print(f'time = {end - start:0.2f} s')
 
-# print(len(car_name), len(mileage), len(prices), len(auto_url),)
+print(len(car_name), len(mileage), len(prices), len(auto_url),)
 
 for i in range(0, len(car_name)):
       dict[f'Car number {i+1}'] = {
                     'Car name': car_name[i],
                     'Mileage': mileage[i],
                     'Price': prices[i],
-                    'url': adresses[i],
+                    'url': auto_url[i],
             }
 
-print(dict)
+# print(dict)
+print(len(dict))
+
+def create_table_auto(table: str, auto: dict):
+    keys = ['ID']
+    for k in auto['Car number 1'].keys():
+        keys.append(k)
+    values = ['Car number 1']
+    for v in auto['Car number 1'].values():
+        values.append(v)
+    print(keys, values)
+
+    create = f'CREATE TABLE {table}('
+    for i in range(len(keys)):
+        create += f'"{keys[i]}" "{values[i]}", '
+    create = create[:-2] + ')'
+    # print(create)
+    connect = sqlite3.connect(f'{table}_data_base.db')
+    cursor = connect.cursor()
+    cursor.execute(create)
+    connect.commit()
 
 
+def iscert_table_auto(table: str, auto: dict):
+    keys = ['ID']
+    for k in auto['Car number 1'].keys():
+        keys.append(k)
+    # print(keys)
+    # print(len(auto))
+    for i in range(0, len(auto)):
+        # print(i)
+        values = [f'Car number {i+1}']
+        for v in auto[f'Car number {i+1}'].values():
+            values.append(v)
+        # print(values)
+        insert = f'INSERT INTO {table}' \
+                 f'("{keys[0]}", "{keys[1]}", "{keys[2]}", "{keys[3]}", "{keys[4]}") ' \
+                 f'VALUES ' \
+                 f'("{values[0]}", "{values[1]}", "{values[2]}", "{values[3]}", "{values[4]}")'
+        # insert = f'INSERT INTO {table} ' \
+        #          f'("{keys[0]}", "{keys[1]}", "{keys[2]}", "{keys[3]}", "{keys[4]}") ' \
+        #          f'VALUES ' \
+        #          f'("Car number 2", "Audi Q5 2016", " 140 тис. км пробіг", "27 000 $", "https://auto.ria.com/uk/auto_audi_q5_33153625.html")'
+        # print(insert)
+        connect = sqlite3.connect(f'{table}_data_base.db')
+        cursor = connect.cursor()
+        cursor.execute(insert)
+        connect.commit()
+
+
+# создаем таблицу в БД
+# create_table_auto('AUDI_autoria', dict)
+# добавляем данные в БД
+# iscert_table_auto('AUDI_autoria', dict)
+
+def get_auto(table: str, auto: str):
+    connect = sqlite3.connect(f'{table}_data_base.db')
+    cursor = connect.cursor()
+    get = f'SELECT "Car name", "Mileage", "Price", "url" FROM {table} WHERE ID="{auto}"'
+    cursor.execute(get)
+    result = cursor.fetchall()
+    return result
+
+
+# res = get_auto('AUDI_autoria', 'Car number 57')
+# print(res)
